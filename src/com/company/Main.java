be2b3699ba9b7f8f3b2c1c629dffb1da4e0f4ace
jiva2104 <jiva2104@gmail.com>
2020-02-05 14:39:54 +0200

    SUPER commit with changes files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("THAT'S MY FIRST CODE!");
+        System.out.println("NEW BRANCH");
     }
 }
