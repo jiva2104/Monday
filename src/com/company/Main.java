2d4265baac7a711b6bdcfac3d93ec9677d1799bf
jiva2104 <jiva2104@gmail.com>
2020-02-05 15:53:18 +0200

    Change text for merge request conflict

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("THAT'S MY FIRST CODE!");
-        System.out.println("NEW BRANCH");
+//bla bla
     }
 }
